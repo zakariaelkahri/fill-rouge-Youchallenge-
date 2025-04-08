7c0905ec2ae91b93a5e2b99876f7cc5e3a07ca0e
zakariaelkahri <zakariakahri2003@gmail.com>
2025-04-08 15:59:37 +0100

    tournament table added also migration done

--- database/migrations/2025_04_08_133748_create_tournaments_table.php
+++ database/migrations/2025_04_08_133748_create_tournaments_table.php
@@ -15,8 +15,31 @@ return new class extends Migration
     {
         Schema::create('tournaments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('organisator_id')->constrained('organisators');
+            $table->string('name');
+            $table->string('photo')->nullable();
+            $table->enum('format',['FC25','VALORANT','CSGO']);
+            $table->string('max_participants');
+            $table->text('reward')->nullable();
+            $table->text('rules')->nullable();
+            $table->enum('status',['upcoming','ongoing','completed'])->default('upcoming');
+            $table->boolean('deleted')->default(0);
+            $table->timestamp('start_date')->nullable();
+            $table->timestamp('end_date')->nullable();
             $table->timestamps();
+            
         });
+//  - tournament_id: int
+// - name: string
+// - photo: string
+// - format: string
+// - start_date: Date
+// - end_date: Date
+// - max_participants: int
+// - status: string
+// - reward: string
+// - rules: string
+// -deleted:bool
     }
 
     /**
